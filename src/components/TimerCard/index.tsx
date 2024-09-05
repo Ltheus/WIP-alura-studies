8a9052214affb47162db1f166bee3d218366cf8a
Ltheus <matheusglopes1@gmail.com>
2024-09-05 19:19:29 -0300

    Started timer functions creation

--- src/components/TimerCard/index.tsx
+++ src/components/TimerCard/index.tsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
+import { ITask } from "../../types/task";
+import { timeToSeconds } from "../../utils/date";
 import Button from "../Button";
 import Timer from "./Timer";
 import style from "./TimerCard.module.scss";
 
-export default function TimerCard() {
+interface timerCardProps {
+  selectedTask: ITask | undefined;
+}
+
+export default function TimerCard(props: timerCardProps) {
+  const [time, setTime] = useState<number>();
+
+  useEffect(() => {
+    if (props.selectedTask?.time) {
+      setTime(timeToSeconds(props.selectedTask.time));
+    }
+  }, [props.selectedTask?.selected]);
+
   return (
     <div className={style.timerCard}>
       <p className={style.title}>Escolha uma tarefa para iniciar</p>
+      <p className={style.title}>{time}</p>
       <div className={style.timerWrapper}>
         <Timer />
       </div>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "../components/Form";
 import List from "../components/List";
 import style from "./App.module.scss";
@@ -6,13 +6,24 @@ import TimerCard from "../components/TimerCard";
 import { ITask } from "../types/task";
 
 function App() {
-  const [tasks, setTasks] = React.useState<ITask[] | []>([]);
+  const [tasks, setTasks] = React.useState<ITask[]>([]);
+  const [selected, setSelected] = useState<ITask>();
+
+  const selectTask = (selectedTask: ITask) => {
+    setSelected(selectedTask);
+    setTasks((oldTasks) =>
+      oldTasks.map((task) => ({
+        ...task,
+        selected: task.id === selectedTask.id ? true : false,
+      }))
+    );
+  };
 
   return (
     <div className={style.AppStyle}>
       <Form setTasks={setTasks} />
-      <List tasks={tasks} />
-      <TimerCard />
+      <List tasks={tasks} selectTask={selectTask} />
+      <TimerCard selectedTask={selected} />
     </div>
   );
 }
