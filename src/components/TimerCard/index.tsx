e82b5e39d77ee522c32ee8878bb18dc8b40f4b82
Ltheus <matheusglopes1@gmail.com>
2024-09-06 16:33:16 -0300

    Implemented timer functions

--- src/components/TimerCard/index.tsx
+++ src/components/TimerCard/index.tsx
@@ -1,31 +1,41 @@
 import { useEffect, useState } from "react";
 import { ITask } from "../../types/task";
 import { timeToSeconds } from "../../utils/date";
-import Button from "../Button";
-import Timer from "./Timer";
+import { Button } from "../Button";
+import { Timer } from "./Timer";
 import style from "./TimerCard.module.scss";
 
 interface timerCardProps {
   selectedTask: ITask | undefined;
+  completeTask: () => void;
 }
 
-export default function TimerCard(props: timerCardProps) {
+export const TimerCard = (props: timerCardProps) => {
   const [time, setTime] = useState<number>();
 
   useEffect(() => {
     if (props.selectedTask?.time) {
       setTime(timeToSeconds(props.selectedTask.time));
     }
-  }, [props.selectedTask?.selected]);
+  }, [props.selectedTask?.selected, props.selectedTask?.time]);
+
+  const countdown = (timer: number = 0) => {
+    setTimeout(() => {
+      if (timer > 0) {
+        setTime(timer - 1);
+        return countdown(timer - 1);
+      }
+      props.completeTask();
+    }, 1000);
+  };
 
   return (
     <div className={style.timerCard}>
       <p className={style.title}>Escolha uma tarefa para iniciar</p>
-      <p className={style.title}>{time}</p>
       <div className={style.timerWrapper}>
-        <Timer />
+        <Timer time={time} />
       </div>
-      <Button>Iniciar</Button>
+      <Button onClick={() => countdown(time)}>Começar</Button>
     </div>
   );
-}
+};
